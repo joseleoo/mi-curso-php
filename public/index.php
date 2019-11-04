e9ba99742b0e88c76173e9bb7e0343b20a041e04
Jose Leonardo Quiñones <desarrollojlq@outlook.es>
2019-11-04 17:25:38 -0500

    DRIVER ENV

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ require_once '../vendor/autoload.php';
 session_start();
 
 $dotenv = Dotenv\Dotenv::create(__DIR__ . '/..');
-// $dotenv->load();
+$dotenv->load();
 
 
 
@@ -19,7 +19,7 @@ use Aura\Router\RouterContainer;
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver'    => 'mysql',
+    'driver'    => getenv('DB_DRIVER'),
     'host'      => getenv('DB_HOST'),
     'database'  => getenv('DB_NAME'),
     'username'  => getenv('DB_USER'),
